3273df13bf2be06e42896cdca615c218cbf38212
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-07-21 11:46:09 +0900

    Prismaテスト

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("aaa");
-	}
-} satisfies ExportedHandler<Env>;
+import { PrismaClient } from "@prisma/client";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", async (c) => {
+	const prisma = new PrismaClient();
+
+	const result = await prisma.user.findMany({
+		select: {
+			id: true
+		}
+	});
+
+	return c.text("Hello Hono!" + JSON.stringify(result));
+});
+
+export default app;
